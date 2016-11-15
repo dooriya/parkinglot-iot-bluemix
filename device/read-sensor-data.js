989d6d502cae2ec6b1ee6aeca2b31e9eccdd1896
dooriya <dol@microsoft.com>
2016-11-15 15:23:08 +0800

    Update read-sensor-data.js

--- device/read-sensor-data.js
+++ device/read-sensor-data.js
@@ -1,3 +1,3 @@
 var hmc = require("hmc5883l-pi");
 var data = hmc.readMag();
-console.log("{\"x\":" + data.x + ",\"y\":" + data.y + ",\"z\":" + data.z + "}");
+console.log("{\"x\":\"" + data.x + "\",\"y\":\"" + data.y + "\",\"z\":\"" + data.z + "\"}");
